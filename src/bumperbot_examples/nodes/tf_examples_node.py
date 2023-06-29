6b8e501483f715865ad3be5cb920485c4aa67714
2002Esala <esalasithumperera@gmail.com>
2023-06-29 12:54:11 +0530

    test

---
+++ src/bumperbot_examples/nodes/tf_examples_node.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import rospy
+from bumperbot_examples.tf_examples import TfExamples
+
+if __name__ == 'main':
+    rospy.init_node('tf_exmples')
+    tfExamples = TfExamples()
+    print("loser")
